ba164434e724008537af0f92e6d2c373b73cc0f9
Monomachus <monomachus85@yahoo.com>
2016-01-14 15:17:13 +0200

    Cat Exercise done

--- build/app.js
+++ build/app.js
@@ -1,59 +1,97 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"C:\\Users\\tpoperecinii\\Documents\\framer_demo\\src\\app.coffee":[function(require,module,exports){
-var hint, i, imageLayer, j, obj, ref, states, textLayer;
+var Background, Card, EmptyHeart, FilledHeart, Heart;
+
+Framer.Defaults.DeviceView = {
+  "deviceScale": -1,
+  "orientation": 0,
+  "contentScale": 1,
+  "deviceType": "iphone-6-silver"
+};
 
 Framer.Device = new Framer.DeviceView();
 
-Framer.Device.deviceType = 'iphone-6-gold-hand';
-
 Framer.Device.setupContext();
 
-imageLayer = new Layer({
+
+/*
+ EXERCISE: Fundamentals
+ 
+ We'll create a basic prototype in which the user
+ can tap a heart to "like" and again to "unlike".
+ The basic image layers have been created and
+ imported for you to start.
+ */
+
+Background = new Layer({
   x: 0,
   y: 0,
-  width: 128,
-  height: 128,
-  image: 'images/Icon.png'
+  width: 750,
+  height: 1334,
+  image: "images/DeviceBackground.png"
 });
 
-imageLayer.center();
-
-states = JSON.parse(Utils.domLoadDataSync("data/states.json"));
-
-for (i = j = 0, ref = states.length; 0 <= ref ? j < ref : j > ref; i = 0 <= ref ? ++j : --j) {
-  imageLayer.states.add((
-    obj = {},
-    obj["State" + i] = states[i],
-    obj
-  ));
-}
-
-imageLayer.states.animationOptions = {
-  curve: 'spring(500,12,0)'
-};
-
-imageLayer.on(Events.Click, function() {
-  return imageLayer.states.next();
+Card = new Layer({
+  x: 37,
+  y: 284,
+  width: 676,
+  height: 655,
+  image: "images/Card.png"
 });
 
-hint = require('./modules/hint')();
-
-textLayer = new Layer({
+EmptyHeart = new Layer({
   x: 0,
-  y: Screen.height - 96,
-  width: Screen.width,
-  height: 64,
-  backgroundColor: 'none',
-  color: 'white',
-  html: "<p style=\"text-align: center; font-size: 48px; line-height: 1.2\">" + hint + "</p>"
+  y: 0,
+  width: 52,
+  height: 51,
+  image: "images/HeartEmpty.png"
 });
 
-textLayer.centerX();
+FilledHeart = new Layer({
+  x: 0,
+  y: 0,
+  width: 52,
+  height: 51,
+  image: "images/HeartFilled.png"
+});
 
 
-},{"./modules/hint":"C:\\Users\\tpoperecinii\\Documents\\framer_demo\\src\\modules\\hint.coffee"}],"C:\\Users\\tpoperecinii\\Documents\\framer_demo\\src\\modules\\hint.coffee":[function(require,module,exports){
-module.exports = function() {
-  return 'Click icon, please!';
-};
+/*
+ Create a "Heart" layer to contain the EmptyHeart and 
+ FilledHeart layers, and make sure to set their superLayer
+ property accordingly.
+ Hint: the Heart layer's x position is 610, y position is 834 
+ and width/height is 52.
+ */
+
+Heart = new Layer({
+  x: 610,
+  y: 834,
+  height: 52,
+  width: 52,
+  backgroundColor: null
+});
+
+EmptyHeart.superLayer = Heart;
+
+FilledHeart.superLayer = Heart;
+
+
+/*
+ Hide the FilledHeart layer by default.
+ */
+
+FilledHeart.visible = false;
+
+
+/*
+ Toggle the visibility of the FilledHeart layer every time
+ the user clicks on the Heart layer.
+ Hint: use Layer.on and Events.Click for this.
+ */
+
+Heart.on(Events.Click, function() {
+  return FilledHeart.visible = !FilledHeart.visible;
+});
 
 
 },{}]},{},["C:\\Users\\tpoperecinii\\Documents\\framer_demo\\src\\app.coffee"])
--- src/app.coffee
+++ src/app.coffee
@@ -1,42 +1,60 @@
-Framer.Device = new Framer.DeviceView()
-Framer.Device.deviceType = 'iphone-6-gold-hand'
-Framer.Device.setupContext()
+Framer.Defaults.DeviceView = {
+  "deviceScale" : -1,
+  "orientation" : 0,
+  "contentScale" : 1,
+  "deviceType" : "iphone-6-silver"
+};
 
-# Welcome to Framer
-# This is just demo code. Feel free to delete it all.
+Framer.Device = new Framer.DeviceView();
+Framer.Device.setupContext();
 
-imageLayer = new Layer
-    x: 0
-    y: 0
-    width: 128
-    height: 128
-    image: 'images/Icon.png'
+###
+ EXERCISE: Fundamentals
+ 
+ We'll create a basic prototype in which the user
+ can tap a heart to "like" and again to "unlike".
+ The basic image layers have been created and
+ imported for you to start.
+###
+Background = new Layer 
+	x:0, y:0, width:750, height:1334, image:"images/DeviceBackground.png"
+Card = new Layer 
+	x:37, y:284, width:676, height:655, image:"images/Card.png"
+EmptyHeart = new Layer 
+	x:0, y:0, width:52, height:51, image:"images/HeartEmpty.png"
+FilledHeart = new Layer 
+	x:0, y:0, width:52, height:51, image:"images/HeartFilled.png"
 
-imageLayer.center()
+###
+ Create a "Heart" layer to contain the EmptyHeart and 
+ FilledHeart layers, and make sure to set their superLayer
+ property accordingly.
+ Hint: the Heart layer's x position is 610, y position is 834 
+ and width/height is 52.
+###
 
-# Define a set of states with names (the original state is 'default')
-states = JSON.parse Utils.domLoadDataSync "data/states.json"
+Heart = new Layer
+    x:610, 
+    y: 834, 
+    height: 52, 
+    width:52,
+    backgroundColor: null
 
-for i in [0...states.length]
-    imageLayer.states.add "State#{i}": states[i]
 
-# Set the default animation options
-imageLayer.states.animationOptions = curve: 'spring(500,12,0)'
+EmptyHeart.superLayer = Heart;
+FilledHeart.superLayer = Heart;
+	
+###
+ Hide the FilledHeart layer by default.
+###
 
-# On a click, go to the next state
-imageLayer.on Events.Click, -> imageLayer.states.next()
+FilledHeart.visible = false;
 
-hint = require('./modules/hint')()
+###
+ Toggle the visibility of the FilledHeart layer every time
+ the user clicks on the Heart layer.
+ Hint: use Layer.on and Events.Click for this.
+###
 
-textLayer = new Layer
-    x: 0
-    y: Screen.height - 96
-    width: Screen.width
-    height: 64
-    backgroundColor: 'none'
-    color: 'white'
-    html: """
-        <p style="text-align: center; font-size: 48px; line-height: 1.2">#{hint}</p>
-    """
-
-textLayer.centerX()
+Heart.on Events.Click, () ->
+    FilledHeart.visible = !FilledHeart.visible;
